09d5171a2aa5886f6ae1c514ca90c0629ae75b12
Alexander Kraus <nr4@z10.info>
2018-08-10 23:49:37 +0200

    added syntax highlighting

--- toy210.py
+++ toy210.py
@@ -29,16 +29,65 @@ import datetime
 from numpy import *
 from struct import *
 
-class ErrorHighlighter(QSyntaxHighlighter) :
-    def __init__(self, lines) :
-        super(ErrorHighlighter, self).__init__()
-        self.lines = lines
+class SyntaxHighlighter(QSyntaxHighlighter) :
+    type_ids = [ 'vec2', 'vec3', 'int', 'vec4', 'mat2', 'mat3', 'mat4', 'void', 'float' ]
+    flow_ids = [ 'in', 'out', 'uniform', 'const' ]
+    syntax_ids = [ 'for', 'if', 'while', 'struct', 'return', 'else' ]
+    operator_ids = [ '\+', '-', '\*', '/', '\+=', '-=', '*=', '/=', '==', '!=', '\,', '\.' ]
+    builtin_ids = [ 'sin', 'cos', 'tan', 'asin', 'acos', 'atan',
+                   'sinh', 'cosh', 'tanh', 'asinh', 'acosh', 'atanh', 
+                   'normalize', 'abs', 'length', 'dot', 'reflect', 'refract',
+                   'mod', 'fract', 'ceil', 'floor', 'round', 'step', 'smoothstep', 'mix',
+                   'max', 'min', 'pow', 'sqrt', 'sign', 'clamp']
+    brace_ids = [ '{', '}', '(', ')', '[', ']' ]
+    
+    color_names = [ 'darkBlue', 'darkMagenta', 'blue', 'red', 'darkGreen', 'orange', 'black', 'lightGray', 'darkGreen', 'purple' ]
+    styles_bold = [ True, True, True, True, True, True, False, False, False, False ]
+    styles_italic = [ False, False, False, False, False, False, False, True, False, False ]
+    formats = []
+    rules = []
+    
+    def __init__(self, document) :
+        QSyntaxHighlighter.__init__(self, document)
+
+        for i in range(10) :
+            color = QColor()
+            color.setNamedColor(self.color_names[i])
+            
+            fmt = QTextCharFormat()
+            fmt.setForeground(color)
+            if self.styles_bold[i] : fmt.setFontWeight(QFont.Bold)
+            fmt.setFontItalic(self.styles_italic[i])
+            
+            self.formats += [ fmt ]
+        
+        rules = []
+        rules += [ (r'//[^\n]*', 0, self.formats[8]) ] # oneline comment
+        rules += [ (r'/[^\*\\]*(\\.[^*/\\]*)*"', 0, self.formats[8]) ] # multiline comment
+        rules += [ (r'#[^\n]*', 0, self.formats[9]) ] # preprocessor
+        rules += [ (r'\b%s\b' % idf, 0, self.formats[0]) for idf in self.type_ids ]
+        rules += [ (r'\b%s\b' % idf, 0, self.formats[1]) for idf in self.flow_ids ]
+        rules += [ (r'\b%s\b' % idf, 0, self.formats[2]) for idf in self.syntax_ids ]
+        rules += [ (r'%s' % idf, 0, self.formats[3]) for idf in self.operator_ids ]
+        rules += [ (r'\b%s\b' % idf, 0, self.formats[4]) for idf in self.builtin_ids ]
+        rules += [ (r'\.[xyz]+', 0, self.formats[5]) ]
+        rules += [ (r'%s' % idf, 0, self.formats[6]) for idf in self.brace_ids ]
+        rules += [ (r'\b[+-]?[0-9]+[lL]?\b', 0, self.formats[9]) ]
+        rules += [ (r'\b[+-]?0[xX][0-9A-Fa-f]+[lL]?\b', 0, self.formats[9]) ]
+        rules += [ (r'\b[+-]?[0-9]+(?:\.[0-9]+)?(?:[eE][+-]?[0-9]+)?\b', 0, self.formats[9]) ]
+        
+        self.rules = [ (QRegExp(s), index, fmt) for (s, index, fmt) in rules ]
         
     def highlightBlock(self, text) :
-        fmt = QTextCharFormat()
-        fmt.setFontWeight(QFont.Bold)
-        fmt.setForeground(Qt.yellow)
-        fmt.setBackground(Qt.red)
+        for expression, nth, format in self.rules:
+            index = expression.indexIn(text, 0)
+        
+            while index >= 0:
+                index = expression.pos(nth)
+                length = len(expression.cap(nth))
+                self.setFormat(index, length, format)
+                index = expression.indexIn(text, index + length)
+        self.setCurrentBlockState(0)
         
 class MainWindow(QWidget):
     def __init__(self):
@@ -189,6 +238,8 @@ uniform float iSampleRate;\n\n"
         
         self.clean = True
         self.filename = ""
+        
+        self.highlight = SyntaxHighlighter(self.editor.document())
     
     def compileShaderSFX(self) :
         log = self.sfxglwidget.newShader(self.sfxprefix + self.editorsfx.toPlainText() + self.sfxsuffix).decode('utf-8')
